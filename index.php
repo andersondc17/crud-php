abcd09dd952290c3798c0fdf588dae0ea97145e0
andersondc17 <45853805+andersondc17@users.noreply.github.com>
2019-04-05 00:18:01 -0500

    segunda version

--- index.php
+++ index.php
@@ -10,34 +10,72 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="css/custom.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+<link rel="stylesheet" href="css/jPages.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/estilos.css">
+<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+
+<script src="js/jPages.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="js/codigo.js"></script>
+
+<script type="text/javascript">
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-28718218-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+</script>
+
+<script>
+  /* when document is ready */
+  $(function() {
+
+    /* initiate plugin */
+    $("div.holder").jPages({
+      containerID : "itemContainer",
+      perPage     : 3,
+      animation   : "fadeInDown",
+      delay       : 50,
+      direction   : "auto"
+    });
+
+    /* on select change */
+    $("select").change(function() {
+      /* get new direction */
+      var newDirection = $(this).val();
+
+      /* destroy jPages and initiate plugin again */
+      $("div.holder").jPages("destroy").jPages({
+        containerID : "itemContainer",
+        perPage     : 5,
+        animation   : "fadeInDown",
+        delay       : 50,
+        direction   : newDirection
+      });
+    });
+  });
+  </script>
+
 
 </head>
 <body>
-    <div class="container">
-        <div class="table-wrapper">
-            <div class="table-title">
-                <div class="row">
-                    <div class="col-sm-8"><h2>Listado de  <b>Clientes</b></h2></div>
-                    <div class="col-sm-4">
-                        <a href="create.php" class="btn btn-info add-new"><i class="fa fa-plus"></i> Agregar cliente</a>
-                    </div>
-                </div>
-            </div>
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Nombres</th>
-                        <th>Teléfono</th>
-                        <th>Dirección</th>
-			            <th>E-mail</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                 
-                <tbody>    
-                <?php 
+    
+
+    <div id="content" class="defaults">
+
+
+<!-- item container -->
+<ul id="itemContainer">
+
+<?php 
 include ('database.php');
 $clientes = new Database();
 $listado=$clientes->read();
@@ -51,22 +89,23 @@ $telefono=$row->telefono;
 $direccion=$row->direccion;
 $email=$row->correo_electronico;
 ?>
-<tr>
-<td><?php echo $nombres;?></td>
-<td><?php echo $telefono;?></td>
-<td><?php echo $direccion;?></td>
-<td><?php echo $email;?></td>
-<td>
-<a href="update.php?id=<?php echo $id;?>" class="edit" title="Editar" data-toggle="tooltip"><i class="material-icons">&#xE254;</i></a>
-<a href="delete.php?id=<?php echo $id;?>" class="delete" title="Eliminar" data-toggle="tooltip"><i class="material-icons">&#xE872;</i></a>
-</td>
-</tr>	
+
+<li><img class="img-lista" src="img/goku.jpg" alt="image">
+
+
 <?php
 }
-?>                </tbody>
-            </table>
-        </div>
-    </div>     
+?>    
+
+
+
+</ul>
+
+<!-- navigation holder -->
+<div class="holder"></div>
+
+</div> <!--! end of #content -->
+
 </body>
 </html>
 
---
+++ js/codigo.js
@@ -0,0 +1,9 @@
+$('document').ready(function(){
+   
+    $('.img-lista').click(function(){
+        alert();
+     
+    });
+
+  
+})
